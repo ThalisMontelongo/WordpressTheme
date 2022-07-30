04332edc6b81975bc9e94c3430dbc85d561268f2
thalis alison <thalisalison@gmail.com>
2022-07-30 16:22:20 -0300

    Meu Tema wordpress

---
+++ CusoDePhp/index.php
@@ -0,0 +1,10 @@
+<?php 
+$idade = 40;
+
+if($idade >= 18) {
+    echo 'Menor de idade';
+} else {
+    echo 'Maior de idade';
+}
+
+?>
---
+++ wp-content/themes/thalistema/index.php
@@ -0,0 +1,14 @@
+/*
+Theme Name: Tema Thalis Alison
+Author: Thalis Dev
+Description: Um Tema adptado de layout Boostrap
+Version: 1.0 
+
+*/<br>
+<br>
+
+
+<?php 
+
+
+?>
